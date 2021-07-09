7abf5a3f778ca2de612bc1bb2e1023458ce87f10
amaben2020 <amaben202phantom@gmail.com>
2021-07-09 05:52:24 +0100

    Created the access token

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+	baseURL: '',
+	username: 'amaben2020',
+	headers: {
+		'Content-Type': 'application/json',
+		Authorization: 'bearer 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',
+	},
+};
+
+export default github;
